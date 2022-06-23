1512c0ce3d49eb1a6529b2e48079fcad8904907c
Dominic Giacona <dominic@exobyte.io>
2022-06-22 22:48:51 -0400

    rendering map and some styling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import axios from 'axios';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
 import './App.css';
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        city: '',
-        cityData: {},
-        error: false,
-        errorMessage: ''
-      }
-    };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: {},
+      error: false,
+      errorMessage: '',
+      cityMap: ''
+    }
+  };
+  
+  handleCityInput = (e) => {
+    this.setState({
+      city: e.target.value
+    });
+  };
 
   handleSubmit = async (event) => {
     event.preventDefault();
       try {
         let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
         console.log(url);
-
-      let cityData = await axios.get(url);
-      console.log(cityData.data[0]);
-      this.setState({
-      cityData: cityData.data[0],
-      error: false
-      });
-    } catch (error) {
-      console.log('error: ', error)
-      console.log('error.message: ', error.message);
+        let cityData = await axios.get(url);
+        let cityMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityData.data[0].lat},${cityData.data[0].lon}&zoom=12`;
+        this.setState({
+          cityData: cityData.data[0],
+          cityMap: cityMap
+        });
+    } 
+    catch (error) {
       this.setState({
         error: true,
         errorMessage: `An Error Occurred: ${error.response.status}`
@@ -37,30 +44,24 @@ class App extends React.Component {
   };
 
 
-  handleCityInput = (e) => {
-    this.setState({
-      city: e.target.value
-    });
-  };
-
-
   render() {
 
     return (
       <>
       <h1>Ready to Explore?</h1>
-      <form onSubmit={this.handleSubmit}>
+      <Form onSubmit={this.handleSubmit}>
         <label>Pick a City:
           <input onChange={this.handleCityInput}type="text"/>
         </label>
-        <button type="submit">Explore!</button>
-      </form>
+        <Button type="submit">Explore!</Button>
+      </Form>
       {this.state.error
         ? <p>{this.state.errorMessage}</p>
         :<ul>
-        <li>{'City Name: ' + this.state.cityData.display_name}</li>
-        <li>{'Latitude: ' + this.state.cityData.lat}</li>
-        <li>{'Longitude: ' + this.state.cityData.lon}</li>
+        <p>{'City Name: ' + this.state.cityData.display_name}</p>
+        <p>{'Latitude: ' + this.state.cityData.lat}</p>
+        <p>{'Longitude: ' + this.state.cityData.lon}</p>
+        <Image class="img" src={this.state.cityMap}></Image>
       </ul>
       }
       </>
